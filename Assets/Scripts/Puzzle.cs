8a72050dcf002c32f127465e06b8ecfb06d8e9e1
IoriBranford <ioribranford@gmail.com>
2019-01-25 23:58:15 -0800

    Puzzle prototype.

---
+++ Assets/Scripts/Puzzle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+	void Start() {
+		foreach (var spriteRenderer in GetComponentsInChildren<SpriteRenderer>()) {
+			var color = spriteRenderer.color;
+			color.a = .25f;
+			spriteRenderer.color = color;
+		}
+	}
+}
---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other) {
+		var isSolution = other.transform.parent.name == "Solution";
+		if (isSolution && other.transform.childCount == 0) {
+			var rigidbody = GetComponent<Rigidbody2D>();
+			rigidbody.bodyType = RigidbodyType2D.Static;
+			var collider = GetComponent<BoxCollider2D>();
+			collider.enabled = false;
+			transform.position = other.transform.position;
+			transform.parent = other.transform;
+		}
+	}
+}
